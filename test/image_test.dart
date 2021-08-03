56634761b1d8377af0a20d1e607f945028f63fdb
Lucas Batista <lucas.batista@megaleios.com>
2021-08-03 17:01:26 -0300

    Adds test

---
+++ test/image_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:moralar_widgets/assets.dart';
+import 'package:moralar_widgets/moralar_widgets.dart';
+
+void main() {
+  group('Image from assets', () {
+    testWidgets('Should render', (tester) async {
+      tester.pumpWidget(MoralarImage.asset(Assets.images.appLogo));
+    });
+  });
+}
