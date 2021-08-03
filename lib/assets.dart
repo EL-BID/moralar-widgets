4f2870453f17e11b9e50c0fae3803b7b5dd1009f
Lucas-megaleios <86004242+Lucas-megaleios@users.noreply.github.com>
2021-08-03 16:46:39 -0300

    Edited fonts and images and created Finalized Card

--- lib/assets.dart
+++ lib/assets.dart
@@ -2,6 +2,7 @@ class Assets {
   static const _path = 'assets';
 
   static _Images get images => _Images(_path);
+  static String get packageName => 'moralar_widgets';
 }
 
 class _Images {
@@ -11,5 +12,10 @@ class _Images {
     _path = basePath + '/images';
   }
 
+  String get agendamento => '$_path/agendamento.png';
   String get appLogo => '$_path/app_logo.png';
+  String get cursos => '$_path/cursos.png';
+  String get enquetes => '$_path/enquetes.png';
+  String get jogos => '$_path/jogos.png';
+  String get videos => '$_path/videos.png';
 }
--- lib/card.dart
+++ lib/card.dart
@@ -48,94 +48,197 @@ class SchedulingCard extends StatelessWidget {
 
     return InkWell(
       onTap: function ?? () {},
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 24),
-        child: Container(
-          height: 140,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.7),
-                // spreadRadius: 1,
-                blurRadius: 3,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.only(right: 16, left: 16, top: 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Reunião TTS',
-                        style: textTheme.headline1?.copyWith(
-                          color: statusColor(status),
-                        ),
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 24),
+        height: 140,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.7),
+              // spreadRadius: 1,
+              blurRadius: 3,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(right: 16, left: 16, top: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Reunião TTS',
+                      style: textTheme.headline1?.copyWith(
+                        color: statusColor(status),
                       ),
-                      SizedBox(height: 8),
-                      rowWidgetText(
-                        Icon(
-                          FontAwesomeIcons.calendar,
-                          size: 16,
-                          color: brownGrey,
-                        ),
-                        '26/09/2020',
-                        textTheme.bodyText1,
+                    ),
+                    SizedBox(height: 8),
+                    rowWidgetText(
+                      Icon(
+                        FontAwesomeIcons.calendar,
+                        size: 16,
+                        color: brownGrey,
                       ),
-                      SizedBox(height: 8),
-                      rowWidgetText(
-                        Icon(
-                          FontAwesomeIcons.clock,
-                          size: 16,
-                          color: brownGrey,
-                        ),
-                        '14:30hrs',
-                        textTheme.bodyText1,
+                      '26/09/2020',
+                      textTheme.bodyText1,
+                    ),
+                    SizedBox(height: 8),
+                    rowWidgetText(
+                      Icon(
+                        FontAwesomeIcons.clock,
+                        size: 16,
+                        color: brownGrey,
                       ),
-                      SizedBox(height: 16),
-                      rowWidgetText(
-                        Center(
-                          child: Container(
-                            width: 16,
-                            height: 16,
-                            decoration: BoxDecoration(
-                              color: statusColor(status),
-                              shape: BoxShape.circle,
-                            ),
+                      '14:30hrs',
+                      textTheme.bodyText1,
+                    ),
+                    SizedBox(height: 16),
+                    rowWidgetText(
+                      Center(
+                        child: Container(
+                          width: 16,
+                          height: 16,
+                          decoration: BoxDecoration(
+                            color: statusColor(status),
+                            shape: BoxShape.circle,
                           ),
                         ),
-                        statusName(status),
-                        textTheme.bodyText2,
                       ),
-                    ],
-                  ),
+                      statusName(status),
+                      textTheme.bodyText2,
+                    ),
+                  ],
                 ),
               ),
-              Container(
-                color: statusColor(status),
-                width: 24,
-              )
-            ],
-          ),
+            ),
+            Container(
+              color: statusColor(status),
+              width: 24,
+            )
+          ],
         ),
       ),
     );
   }
+}
 
-  Widget rowWidgetText(Widget widget, String text, TextStyle? style) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        widget,
-        SizedBox(width: 8),
-        Flexible(child: Text(text, style: style))
-      ],
+class FinalizedCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final status = 3;
+    final textTheme = Theme.of(context).textTheme;
+    final brownGrey = Color(0xFFb2b2b2);
+
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 24),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.7),
+              // spreadRadius: 1,
+              blurRadius: 3,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              color: statusColor(status),
+              width: double.infinity,
+              height: 24,
+            ),
+            Container(
+              padding: EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Reunião TTS',
+                    style: textTheme.headline1?.copyWith(
+                      color: statusColor(status),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  rowWidgetText(
+                    Icon(
+                      FontAwesomeIcons.calendar,
+                      size: 16,
+                      color: brownGrey,
+                    ),
+                    '26/09/2020',
+                    textTheme.bodyText1,
+                  ),
+                  SizedBox(height: 8),
+                  rowWidgetText(
+                    Icon(
+                      FontAwesomeIcons.clock,
+                      size: 16,
+                      color: brownGrey,
+                    ),
+                    '14:30hrs',
+                    textTheme.bodyText1,
+                  ),
+                  SizedBox(height: 16),
+                  rowWidgetText(
+                    Center(
+                      child: Container(
+                        width: 16,
+                        height: 16,
+                        decoration: BoxDecoration(
+                          color: statusColor(status),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    statusName(status),
+                    textTheme.bodyText2,
+                  ),
+                  SizedBox(height: 32),
+                  Center(
+                    child: Text(
+                      'Descrição',
+                      style: textTheme.headline1?.copyWith(
+                        color: statusColor(status),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Center(
+                    child: Text(
+                      'TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto',
+                      style: textTheme.bodyText1,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget rowWidgetText(Widget widget, String text, TextStyle? style) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      widget,
+      SizedBox(width: 8),
+      Flexible(child: Text(text, style: style))
+    ],
+  );
 }
