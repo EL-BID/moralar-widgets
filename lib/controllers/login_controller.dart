2a4c3c9c9515edb003224d0d7f5f31f89c3a2f46
Lucas Batista <lucas.batista@megaleios.com>
2021-08-11 18:06:13 -0300

    Refactor

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -2,7 +2,7 @@ part of moralar_widgets;
 
 class LoginController extends GetxController {
   final checked = false.obs;
-  final credentials = DocumentCredentials(cpf: '', password: '');
+  final credentials = DocumentCredentials();
 
   Future<void> signIn() async {
     await MegaFlutter.instance.auth.signIn(credentials);
--- lib/models/document_credentials.dart
+++ lib/models/document_credentials.dart
@@ -1,11 +1,6 @@
 part of moralar_widgets;
 
 class DocumentCredentials extends Credentials {
-  String cpf;
-  String password;
-
-  DocumentCredentials({
-    required this.cpf,
-    required this.password,
-  });
+  late String cpf;
+  late String password;
 }
