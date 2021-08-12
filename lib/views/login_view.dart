1f8b2a21627112263e8a8ee773e6259ffdb16bc5
Lucas-megaleios <86004242+Lucas-megaleios@users.noreply.github.com>
2021-08-12 18:21:01 -0300

    Fixed Login View

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -25,67 +25,75 @@ class LoginView extends GetView<LoginController> {
           child: Form(
             key: _cpfFormKey,
             child: Container(
+              height: MediaQuery.of(context).size.height - 100,
               padding: const EdgeInsets.symmetric(horizontal: 24),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 128),
-                  Container(
-                    width: double.infinity,
-                    height: 52,
-                    child: MoralarImage.asset(Assets.images.appLogo),
-                  ),
-                  const SizedBox(height: 128),
-                  MoralarTextField(
-                    label: 'CPF',
-                    hint: '123.123.123-12',
-                    formats: [Formats.cpfMaskFormatter],
-                    keyboard: TextInputType.number,
-                    validators: [
-                      Validatorless.cpf('CPF Inválido'),
-                      Validatorless.required('Preencha esse campo'),
-                    ],
-                    onSaved: (input) =>
-                        controller.credentials.cpf = _unmaskCpf(input!),
-                  ),
-                  const SizedBox(height: 128),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  Expanded(
+                    child: Column(
                       children: [
-                        Obx(
-                          () => LoginCheckBox(
-                            checked: controller.checked.value,
-                            function: () => controller.checked.value =
-                                !controller.checked.value,
+                        const SizedBox(height: 128),
+                        Container(
+                          width: double.infinity,
+                          height: 52,
+                          child: MoralarImage.asset(Assets.images.appLogo),
+                        ),
+                        const SizedBox(height: 128),
+                        MoralarTextField(
+                          label: 'CPF',
+                          hint: '123.123.123-12',
+                          formats: [Formats.cpfMaskFormatter],
+                          keyboard: TextInputType.number,
+                          validators: [
+                            Validatorless.cpf('CPF Inválido'),
+                            Validatorless.required('Preencha esse campo'),
+                          ],
+                          onSaved: (input) =>
+                              controller.credentials.cpf = _unmaskCpf(input!),
+                        ),
+                        const SizedBox(height: 128),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Obx(
+                                () => LoginCheckBox(
+                                  checked: controller.checked.value,
+                                  function: () => controller.checked.value =
+                                      !controller.checked.value,
+                                ),
+                              ),
+                              const SizedBox(width: 16),
+                              TermsUse(),
+                            ],
                           ),
                         ),
-                        const SizedBox(width: 16),
-                        TermsUse(),
+                        const SizedBox(height: 32),
+                        Obx(() {
+                          final checked = controller.checked.value;
+                          return MoralarButton(
+                            onPressed: () {
+                              if (checked &&
+                                  _cpfFormKey.currentState!.validate()) {
+                                _cpfFormKey.currentState!.save();
+                                pageController.jumpToPage(1);
+                              }
+                            },
+                            child: Container(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Continuar',
+                                style: textTheme.button,
+                              ),
+                            ),
+                          );
+                        }),
+                        const SizedBox(height: 16),
                       ],
                     ),
                   ),
-                  const SizedBox(height: 32),
-                  Obx(() {
-                    final checked = controller.checked.value;
-                    return MoralarButton(
-                      onPressed: () {
-                        if (checked && _cpfFormKey.currentState!.validate()) {
-                          _cpfFormKey.currentState!.save();
-                          pageController.jumpToPage(1);
-                        }
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Continuar',
-                          style: textTheme.button,
-                        ),
-                      ),
-                    );
-                  }),
-                  const SizedBox(height: 16),
                   const MoralarPageController(isFirstPage: true),
                 ],
               ),
@@ -115,101 +123,111 @@ class LoginView extends GetView<LoginController> {
           child: Form(
             key: _passwordFormKey,
             child: Container(
+              height: MediaQuery.of(context).size.height - 100,
               padding: const EdgeInsets.symmetric(horizontal: 24),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 128),
-                  Container(
-                    width: double.infinity,
-                    height: 52,
-                    child: MoralarImage.asset(Assets.images.appLogo),
-                  ),
-                  const SizedBox(height: 128),
-                  Visibility(
-                    visible: MoralarWidgets.instance.userType == UserType.tts,
-                    child: MoralarTextField(
-                      label: 'CPF',
-                      hint: '123.123.123-12',
-                      formats: [Formats.cpfMaskFormatter],
-                      keyboard: TextInputType.number,
-                      validators: [
-                        Validatorless.cpf('CPF Inválido'),
-                        Validatorless.required('Preencha esse campo'),
-                      ],
-                      onSaved: (cpf) =>
-                          controller.credentials.cpf = _unmaskCpf(cpf!),
-                    ),
-                  ),
-                  MoralarTextField(
-                    label: 'Senha',
-                    hint: 'Escreva uma senha',
-                    keyboard: TextInputType.visiblePassword,
-                    validators: [
-                      Validatorless.min(
-                        6,
-                        'A senha precisa conter 6 caracteres',
-                      ),
-                      Validatorless.required('Preencha esse campo'),
-                    ],
-                    isPassword: true,
-                    onSaved: (password) {
-                      controller.credentials.password = password!;
-                    },
-                  ),
-                  const SizedBox(height: 128),
-                  MoralarButton(
-                    onPressed: () async {
-                      if (_passwordFormKey.currentState!.validate()) {
-                        _passwordFormKey.currentState!.save();
-                        await controller.signIn();
-                        if (controller.hasError.value) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(controller.errorMessage.value),
-                              backgroundColor: Colors.red,
-                            ),
-                          );
-                        }
-                        if (MegaFlutter.instance.auth.currentUser != null) {
-                          onSignedIn();
-                        }
-                      }
-                    },
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Continuar',
-                        style: textTheme.button,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 32),
-                  MoralarOutlinedButton(
-                    color: Theme.of(context).focusColor,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  Expanded(
+                    child: Column(
                       children: [
-                        Expanded(
+                        const SizedBox(height: 128),
+                        Container(
+                          width: double.infinity,
+                          height: 52,
+                          child: MoralarImage.asset(Assets.images.appLogo),
+                        ),
+                        const SizedBox(height: 128),
+                        Visibility(
+                          visible:
+                              MoralarWidgets.instance.userType == UserType.tts,
+                          child: MoralarTextField(
+                            label: 'CPF',
+                            hint: '123.123.123-12',
+                            formats: [Formats.cpfMaskFormatter],
+                            keyboard: TextInputType.number,
+                            validators: [
+                              Validatorless.cpf('CPF Inválido'),
+                              Validatorless.required('Preencha esse campo'),
+                            ],
+                            onSaved: (cpf) =>
+                                controller.credentials.cpf = _unmaskCpf(cpf!),
+                          ),
+                        ),
+                        MoralarTextField(
+                          label: 'Senha',
+                          hint: 'Escreva uma senha',
+                          keyboard: TextInputType.visiblePassword,
+                          validators: [
+                            Validatorless.min(
+                              6,
+                              'A senha precisa conter 6 caracteres',
+                            ),
+                            Validatorless.required('Preencha esse campo'),
+                          ],
+                          isPassword: true,
+                          onSaved: (password) {
+                            controller.credentials.password = password!;
+                          },
+                        ),
+                        const SizedBox(height: 128),
+                        MoralarButton(
+                          onPressed: () async {
+                            if (_passwordFormKey.currentState!.validate()) {
+                              _passwordFormKey.currentState!.save();
+                              await controller.signIn();
+                              if (controller.hasError.value) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content:
+                                        Text(controller.errorMessage.value),
+                                    backgroundColor: Colors.red,
+                                  ),
+                                );
+                              }
+                              if (MegaFlutter.instance.auth.currentUser !=
+                                  null) {
+                                onSignedIn();
+                              }
+                            }
+                          },
                           child: Container(
                             alignment: Alignment.center,
                             child: Text(
-                              'Não lembra a senha? Recuperar.',
-                              style: textTheme.button?.copyWith(
-                                color: Theme.of(context).focusColor,
-                                fontWeight: FontWeight.normal,
-                              ),
+                              'Continuar',
+                              style: textTheme.button,
                             ),
                           ),
                         ),
-                        Icon(
-                          FontAwesomeIcons.angleRight,
+                        const SizedBox(height: 32),
+                        MoralarOutlinedButton(
                           color: Theme.of(context).focusColor,
-                        )
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    'Não lembra a senha? Recuperar.',
+                                    style: textTheme.button?.copyWith(
+                                      color: Theme.of(context).focusColor,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Icon(
+                                FontAwesomeIcons.angleRight,
+                                color: Theme.of(context).focusColor,
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 16),
                       ],
                     ),
                   ),
-                  const SizedBox(height: 16),
                   const MoralarPageController(isFirstPage: false),
                 ],
               ),
@@ -220,7 +238,7 @@ class LoginView extends GetView<LoginController> {
     }
 
     return PageView(
-      // physics: const NeverScrollableScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
       controller: pageController,
       children: [
         _cpfLogin(),
