a3e171268d967f8d9576ad651ac3cf7864529ac5
Lucas-megaleios <lucas@megaleios.com>
2021-09-30 14:33:37 -0300

    Fixed height in Family Card

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -1501,7 +1501,7 @@ class FamilyCard extends StatelessWidget {
       onTap: function ?? () {},
       child: Container(
         margin: const EdgeInsets.only(bottom: 24),
-        height: 164,
+        height: 196,
         width: double.infinity,
         decoration: BoxDecoration(
           color: Colors.white,
